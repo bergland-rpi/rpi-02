c9f3ab869bbf750591d2d413a22bd00b77b83d9d
bergland.rpi <bergland.rpi@gmail.com>
2017-03-02 11:16:49 -0500

    files

---
+++ pull_restart.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+hn=$(hostname)
+cd /home/pi/$hn
+git pull
+
+if pgrep -f "masterprogram.py" > /dev/null
+then
+    echo "Running"
+else
+    echo "Stopped"
+    sudo python /home/pi/$hn/masterprogram.py /home/pi/$hn/config.txt
+    sudo python /home/pi/$hn/restart_email.py
+fi
